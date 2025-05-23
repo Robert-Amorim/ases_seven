50790b5d33d6dba008a5b9e36686bb85d5b7101b
AmorimDirecty <robertamorimbot@gmail.com>
2025-05-23 04:35:39 -0300

    fix: home

--- ases_seven_front_end/src/pages/index.jsx
+++ ases_seven_front_end/src/pages/index.jsx
@@ -1,4 +1,4 @@
 export {default as MyChurch} from "./MyChurch";
 export {default as Departments} from "./Departments";
-export {default as Home} from "./home";
+export {default as Home} from "./Home";
 export {default as Profile} from "./Profile";
